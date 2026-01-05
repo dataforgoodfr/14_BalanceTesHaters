59e66ca7d5751d651c057f02338830ddf6f473b5
njouanin <nicolas.jouanin@gmail.com>
2026-01-05 21:41:53 +0100

    Fix typo

--- browser-extension/entrypoints/popup/App.tsx
+++ browser-extension/entrypoints/popup/App.tsx
@@ -4,62 +4,62 @@ import { getCurrentTab } from "../shared/utils/getCurrentTab";
 import { parseSocialNetworkUrl } from "../shared/social-network-url";
 
 const sendScrapMessage = () => {
-  browser.runtime.sendMessage({ msgType: "scrap-active-tab" });
+    browser.runtime.sendMessage({ msgType: "scrap-active-tab" });
 };
 
 const reportPageUrl = browser.runtime.getURL("/report-page.html");
 
 function App() {
-  const [currentTab, setCurrentTab] = useState<Browser.tabs.Tab | undefined>(
-    undefined
-  );
-  useEffect(() => {
-    getCurrentTab().then((tab) => {
-      setCurrentTab(tab);
-    });
-  });
-
-  const parsedUrl = useMemo(() => {
-    return (
-      currentTab?.url !== undefined && parseSocialNetworkUrl(currentTab.url)
+    const [currentTab, setCurrentTab] = useState<Browser.tabs.Tab | undefined>(
+        undefined
     );
-  }, [currentTab]);
+    useEffect(() => {
+        getCurrentTab().then((tab) => {
+            setCurrentTab(tab);
+        });
+    });
 
-  return (
-    <>
-      <h1>BTH</h1>
+    const parsedUrl = useMemo(() => {
+        return (
+            currentTab?.url !== undefined && parseSocialNetworkUrl(currentTab.url)
+        );
+    }, [currentTab]);
 
-      {!parsedUrl && (
-        <div className="card">
-          Pour capturé des commentaires et les analyser naviguez vers une
-          publication d'un réseau social supporté (youtube, instagram...) puis
-          ouvrez l'extension à nouveau.
-        </div>
-      )}
-      {parsedUrl && (
-        <div className="card">
-          <p>
-            Vous êtes sur un {parsedUrl.type} {parsedUrl.socialNetwork}. Pour
-            capturé les commentaires et les analyser cliqué sur le bouton.
-          </p>
+    return (
+        <>
+            <h1>BTH</h1>
 
-          <button onClick={() => sendScrapMessage()}>
-            ⏺️ Capturer les commentaires
-          </button>
-          <p>
-            ⚠️ Une fois le bouton cliqué l'extension va prendre le contrôle de
-            la page pour effectuer la capture.
-          </p>
-        </div>
-      )}
+            {!parsedUrl && (
+                <div className="card">
+                    Pour capturer des commentaires et les analyser naviguez vers une
+                    publication d'un réseau social supporté (youtube, instagram...) puis
+                    ouvrez l'extension à nouveau.
+                </div>
+            )}
+            {parsedUrl && (
+                <div className="card">
+                    <p>
+                        Vous êtes sur un {parsedUrl.type} {parsedUrl.socialNetwork}. Pour
+                        capturer les commentaires et les analyser cliquez sur le bouton.
+                    </p>
 
-      <div className="card">
-        <a href={reportPageUrl} target="bth-report-page">
-          Afficher les résultats précedents
-        </a>
-      </div>
-    </>
-  );
+                    <button onClick={() => sendScrapMessage()}>
+                        ⏺️ Capturer les commentaires
+                    </button>
+                    <p>
+                        ⚠️ Une fois le bouton cliqué l'extension va prendre le contrôle de
+                        la page pour effectuer la capture.
+                    </p>
+                </div>
+            )}
+
+            <div className="card">
+                <a href={reportPageUrl} target="bth-report-page">
+                    Afficher les résultats précedents
+                </a>
+            </div>
+        </>
+    );
 }
 
 export default App;
