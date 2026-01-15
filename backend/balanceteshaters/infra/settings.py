a1491e913fcad8ca03d3958833ca14ad5cad7fa0
njouanin <nicolas.jouanin@gmail.com>
2026-01-15 21:31:36 +0100

    Clean config

--- backend/balanceteshaters/infra/settings.py
+++ backend/balanceteshaters/infra/settings.py
@@ -4,25 +4,6 @@ from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict, YamlConfigSettingsSource
 
 
-class Argon2Settings(BaseModel):
-    time_cost: int
-    memory_cost: int
-    parallelism: int
-    hash_len: int
-    salt_len: int
-
-
-class JwtSettings(BaseModel):
-    algorithm: str
-    secret_key: str
-    token_expire_minutes: int
-
-
-class AuthSettings(BaseModel):
-    argon2: Argon2Settings
-    jwt: JwtSettings
-
-
 class DbSettings(BaseModel):
     pg_dsn: PostgresDsn
     engine_echo: bool
@@ -31,7 +12,6 @@ class DbSettings(BaseModel):
 class Settings(BaseSettings):
     db: DbSettings
     logging_configuration_file: pathlib.Path
-    auth: AuthSettings
 
     model_config = SettingsConfigDict(yaml_file="configuration.yml")
 
