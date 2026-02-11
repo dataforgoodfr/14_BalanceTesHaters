fa62fa5cb0454f2c6634a181a4d3164735a0cd6f
njouanin <nicolas.jouanin@gmail.com>
2026-02-11 21:30:18 +0100

    Fix format

--- browser-extension/src/entrypoints/background/index.ts
+++ browser-extension/src/entrypoints/background/index.ts
@@ -7,59 +7,59 @@ import { isScreenshotSenderTab } from "../../shared/native-screenshoting/message
 import { Post } from "../../shared/model/post";
 
 export default defineBackground(() => {
-    console.log("Hello background!", { extensionId: browser.runtime.id });
+  console.log("Hello background!", { extensionId: browser.runtime.id });
 
-    function handleMessages(
-        message: unknown,
-        sender: Browser.runtime.MessageSender,
-        sendResponse: (response: unknown) => void,
-    ) {
-        console.debug("Background - Message received:", message, sender);
+  function handleMessages(
+    message: unknown,
+    sender: Browser.runtime.MessageSender,
+    sendResponse: (response: unknown) => void,
+  ) {
+    console.debug("Background - Message received:", message, sender);
 
-        if (isScrapActiveTabMessage(message)) {
-            scrapActiveTab();
-            return;
-        } else if (isScreenshotSenderTab(message)) {
-            screenshotSenderTab(sender).then((data) => {
-                sendResponse(data);
-            });
-            return true;
-        }
+    if (isScrapActiveTabMessage(message)) {
+      scrapActiveTab();
+      return;
+    } else if (isScreenshotSenderTab(message)) {
+      screenshotSenderTab(sender).then((data) => {
+        sendResponse(data);
+      });
+      return true;
     }
-    console.log("Background - registering listener");
-    browser.runtime.onMessage.addListener(handleMessages);
+  }
+  console.log("Background - registering listener");
+  browser.runtime.onMessage.addListener(handleMessages);
 });
 
 async function postToBackend(post: Post) {
-    console.debug("Background - Posting post to backend");
-    await fetch("http://localhost:8000/ml/post", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(post),
-    });
+  console.debug("Background - Posting post to backend");
+  await fetch("http://localhost:8000/ml/post", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
 }
 
 async function scrapActiveTab() {
-    const tab = await getCurrentTab();
+  const tab = await getCurrentTab();
 
-    if (tab) {
-        console.debug("Background - Scraping post from active tab ", {
-            tabId: tab.id,
-            url: tab.url,
-        });
-        const socialNetworkPost = await scrapPostFromTab(tab);
-        console.debug(
-            "Background - storing post to local storage",
-            socialNetworkPost,
-        );
-        await storePost(socialNetworkPost);
+  if (tab) {
+    console.debug("Background - Scraping post from active tab ", {
+      tabId: tab.id,
+      url: tab.url,
+    });
+    const socialNetworkPost = await scrapPostFromTab(tab);
+    console.debug(
+      "Background - storing post to local storage",
+      socialNetworkPost,
+    );
+    await storePost(socialNetworkPost);
 
-        console.debug(
-            "Background - sending post to backend for analysis",
-            socialNetworkPost,
-        );
-        await postToBackend(socialNetworkPost);
-    }
+    console.debug(
+      "Background - sending post to backend for analysis",
+      socialNetworkPost,
+    );
+    await postToBackend(socialNetworkPost);
+  }
 }
