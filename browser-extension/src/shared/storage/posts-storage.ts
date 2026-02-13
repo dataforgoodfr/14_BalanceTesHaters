2421f8bebb8ad9161476e3178b626cf7944aeb7b
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-02-13 16:24:35 +0100

    format

--- browser-extension/src/shared/storage/posts-storage.ts
+++ browser-extension/src/shared/storage/posts-storage.ts
@@ -9,9 +9,13 @@ export async function storePost(post: Post) {
   await browser.storage.local.set({ posts: newPosts });
 }
 
-export async function setPostBackendId(postId: string,  scrapedAt: string, backendId: string) {
+export async function setPostBackendId(
+  postId: string,
+  scrapedAt: string,
+  backendId: string,
+) {
   const posts: Post[] = await getPosts();
-  const updatedPosts = posts.map(post => {
+  const updatedPosts = posts.map((post) => {
     // Set the backendId of the post matching the postId and scrapedAt date
     if (post.postId === postId && post.scrapedAt === scrapedAt) {
       return { ...post, backendId };
