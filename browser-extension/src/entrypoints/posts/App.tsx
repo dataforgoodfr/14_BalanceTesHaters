2b23e69bcedf8b769cbead51552afbdb7ad005a9
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-02-20 16:08:01 +0100

    feat: add side panel

--- browser-extension/src/entrypoints/posts/App.tsx
+++ browser-extension/src/entrypoints/posts/App.tsx
@@ -7,18 +7,22 @@ import { DebugPage } from "./DebugPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 import HomePage from "./Home/HomePage";
+import SidePanelMenu from "./SidePanelMenu";
 
 export default function App() {
   useInitializeTheme();
   return (
     <QueryClientProvider client={queryClient}>
-         <HashRouter>
-        <Routes>
-          <Route index path="/" element={<HomePage />} />
-          <Route index path="/posts" element={<PostListPage />} />
-          <Route path="/debug" element={<DebugPage />} />
-          <Route path="/:postId/:scrapedAt" element={<PostDetailPage />} />
-        </Routes>
+      <HashRouter>
+        <div className="flex h-screen">
+          <SidePanelMenu />
+          <Routes>
+            <Route index path="/" element={<HomePage />} />
+            <Route index path="/posts" element={<PostListPage />} />
+            <Route path="/debug" element={<DebugPage />} />
+            <Route path="/:postId/:scrapedAt" element={<PostDetailPage />} />
+          </Routes>
+        </div>
       </HashRouter>
     </QueryClientProvider>
   );
---
+++ browser-extension/src/entrypoints/posts/SidePanelMenu.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Link } from "react-router";
+
+function SidePanelMenu() {
+  return (
+    <div className="flex flex-col bg-gray-100 p-4 w-1/4">
+      <div className="text-lg font-bold my-4">Balance tes haters</div>
+      <Button className="text-sm mb-4">
+        <Plus />
+        Créer un rapport
+      </Button>
+      <Button variant="ghost">
+        <Link to="/">Vue d&apos;ensemble</Link>
+      </Button>
+      <Button variant="ghost">
+        <Link to="/posts">Publications analysées</Link>
+      </Button>
+      <Button variant="ghost">
+        <Link to="/">Aide et ressources</Link>
+      </Button>
+    </div>
+  );
+}
+
+export default SidePanelMenu;
