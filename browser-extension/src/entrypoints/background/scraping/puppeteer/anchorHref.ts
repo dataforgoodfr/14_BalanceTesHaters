96a9bf9a42e9434370ada7792504fdccc8120b72
kerrerain <8094424+kerrerain@users.noreply.github.com>
2026-02-20 08:18:32 +0100

    Remove dependency to puppeteer

--- browser-extension/src/entrypoints/background/scraping/puppeteer/anchorHref.ts
+++
@@ -1,7 +0,0 @@
-import { ElementHandle } from "puppeteer-core/lib/esm/puppeteer/puppeteer-core-browser.js";
-
-export async function anchorHref(element: ElementHandle): Promise<string> {
-  const anchorElement: ElementHandle<HTMLAnchorElement> =
-    await element.toElement("a");
-  return await anchorElement.evaluate((e) => e.href);
-}
