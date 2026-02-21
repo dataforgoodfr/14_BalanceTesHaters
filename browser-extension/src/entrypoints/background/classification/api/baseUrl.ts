31c6d267bea9d3cc0a6de745ccef0e629d613461
njouanin <nicolas.jouanin@gmail.com>
2026-02-21 21:17:49 +0100

    Utilisation du serveur de staging par défaut.

--- browser-extension/src/entrypoints/background/classification/api/baseUrl.ts
+++ browser-extension/src/entrypoints/background/classification/api/baseUrl.ts
@@ -1,2 +1,2 @@
 export const apiBaseUrl =
-  import.meta.env.VITE_BACKEND_API_BASE_URL || "http://localhost:8000";
+    import.meta.env.VITE_BACKEND_API_BASE_URL || "http://balanceteshaters-app.services.d4g.fr:8000";
