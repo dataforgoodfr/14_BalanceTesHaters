1cd1da353ff5b34fc05ec80f250c5441905eca9a
njouanin <nicolas.jouanin@gmail.com>
2026-02-22 13:52:32 +0100

    Setup coolify staging

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,17 +1,17 @@
-from logging.config import fileConfig
 import asyncio
-from sqlalchemy import pool
-from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from logging.config import fileConfig
 
 from alembic import context
 from balanceteshaters.infra.settings import Settings
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 settings = Settings()
-config.set_main_option("sqlalchemy.url", str(settings.db.pg_dsn))
+config.set_main_option("sqlalchemy.url", str(settings.pg_dsn))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -28,6 +28,7 @@ target_metadata = None
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
 
 def do_run_migrations(connection: Connection) -> None:
     context.configure(connection=connection, target_metadata=target_metadata)
@@ -52,6 +53,7 @@ async def run_async_migrations() -> None:
         await connection.run_sync(do_run_migrations)
 
     await connectable.dispose()
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
