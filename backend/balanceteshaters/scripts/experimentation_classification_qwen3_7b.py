cf0482a5129534b833974b4af103ca737a7641a2
Grégoire Co <gregoire.corbiere@gmail.com>
2026-02-25 11:51:36 +0100

    make model name argument and variable

--- backend/balanceteshaters/scripts/experimentation_classification_qwen3_7b.py
+++ backend/balanceteshaters/scripts/experimentation_classification_qwen3_7b.py
@@ -5,12 +5,21 @@ Created on Wed Feb 18 18:20:41 2026
 @author: cindy
 """
 import os
+import argparse
 from dotenv import load_dotenv
 import re
 from ollama import chat
 from compute_annotation_stats import AnnotatedCategory, AnnotationConfidence, Annotation, NocoDBService
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        default="qwen3:1.7b",
+        help="ollama model name to use for classification",
+    )
+    args = parser.parse_args()
+
     load_dotenv()
     nocodb_base_url: str = os.environ["NOCODB_BASE_URL"]
     nocodb_annotation_table_id: str = os.environ["NOCODB_ANNOTATION_TABLE_ID"]
@@ -26,7 +35,7 @@ if __name__ == "__main__":
     data=service.get_annotations()
     for record in data:
         response = chat(
-            model='qwen3:1.7b',
+            model=args.model,
             messages=[{'role': 'user', 'content': "Classifie le prompt dans une des catégories et ne répond qu'avec la catégorie choisie, aucun texte autre que le nom de catégorie : 'ABSENCE_DE_CYBERHARCELEMENT', 'CYBERHARCELEMENT_DEFINITION_GENERALE', 'CYBERHARCELEMENT_A_CARACTERE_SEXUEL, 'MENACES', ''INCITATION_AU_SUICIDE', 'INJURE_ET_DIFFAMATION_PUBLIQUE', 'DOXXING', ''INCITATION_A_LA_HAINE'. Prompt à classifier :"+str(record.comment)}],
         )
         content=response.message.content
