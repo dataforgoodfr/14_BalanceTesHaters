55d648e0456aaa9b8428dd4373363d40f1c9cd97
njouanin <nicolas.jouanin@gmail.com>
2026-02-25 13:49:13 +0100

    Ajout d'un token pour vérifier l'accès aux API critiques

--- backend/balanceteshaters/infra/settings.py
+++ backend/balanceteshaters/infra/settings.py
@@ -8,5 +8,6 @@ class Settings(BaseSettings):
     pg_dsn: PostgresDsn
     engine_echo: bool = False
     logging_configuration_file: pathlib.Path
+    api_token: str
 
     model_config = SettingsConfigDict(env_prefix="BTH_")
