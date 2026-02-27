5dd9d748afa175b72fd3f5c6dc36e93fa588865f
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-02-27 17:39:15 +0100

    remove unused var

--- browser-extension/src/entrypoints/posts/Home/HarassmentTrendChard.tsx
+++ browser-extension/src/entrypoints/posts/Home/HarassmentTrendChard.tsx
@@ -1,16 +1,7 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import WorkInProgress from "../WorkInProgress";
-import { PostSnapshot } from "@/shared/model/PostSnapshot";
 
-type HarassmentTrendChartProps = {
-  posts: PostSnapshot[] | undefined;
-  isLoading: boolean;
-};
-
-function HarassmentTrendChart({
-  posts,
-  isLoading,
-}: Readonly<HarassmentTrendChartProps>) {
+function HarassmentTrendChart() {
   return (
     <Card className="w-full relative">
       <CardHeader>Évolution de l&apos;harcèlement</CardHeader>
