9148bb5b22f42e608ef062f2692029e0296390a6
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-02-27 17:39:15 +0100

    remove unused var

--- browser-extension/src/entrypoints/posts/Home/CategoryDistribution.tsx
+++ browser-extension/src/entrypoints/posts/Home/CategoryDistribution.tsx
@@ -1,16 +1,7 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import WorkInProgress from "../WorkInProgress";
-import { PostSnapshot } from "@/shared/model/PostSnapshot";
 
-type CategoryDistributionProps = {
-  posts: PostSnapshot[] | undefined;
-  isLoading: boolean;
-};
-
-function CategoryDistribution({
-  posts,
-  isLoading,
-}: Readonly<CategoryDistributionProps>) {
+function CategoryDistribution() {
   return (
     <Card className="w-full relative">
       <CardHeader>Répartition par catégories</CardHeader>
