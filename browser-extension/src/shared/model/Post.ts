9c996911c64816c1de4bddb3ca9776d17d1014d1
Sébastien Arod <sebastien.arod@gmail.com>
2026-02-28 11:09:46 +0100

    WIP

--- browser-extension/src/shared/model/Post.ts
+++ browser-extension/src/shared/model/Post.ts
@@ -1,6 +1,7 @@
 import { build } from "wxt";
 import { CommentSnapshot, PostSnapshot } from "./PostSnapshot";
 import { id } from "date-fns/locale";
+import { buildCommentsFromSnapshots } from "./buildCommentsFromSnapshots";
 
 /**
  * Merged view of Post Snapshot
@@ -19,7 +20,7 @@ type Post = Pick<
   comments: PostComment[];
 };
 
-type PostComment = Pick<
+export type PostComment = Pick<
   CommentSnapshot,
   | "commentId"
   | "author"
@@ -68,70 +69,4 @@ export function buildPostFromSnapshots(snapshots: PostSnapshot[]): Post {
     title: latest.title,
     comments: buildCommentsFromSnapshots(snapshots.map((s) => s.comments)),
   };
-}
-
-### 
- PostSnapshot1
-  => Commentaire 1
-  => Commentaire 2
-  => Commentaire 3 
-
-PostSnapshot2
-  => Commentaire 1
-  => Commentaire 2 => Edité
-  => Commentaire 3 => Supprimé
-  => Commentaire 4 
-
-Post
-  => Commentaire 1
-  => Commentaire 2
-  => Commentaire 2 v2
-  => Commentaire 3 (supprimé) 
-  => Commentaire 4
-
-function buildCommentsFromSnapshots(
-  allCommentSnapshots: CommentSnapshot[][],
-): PostComment[] {
-  const sortedCommentIds= [];
-  
-  allCommentSnapshots.flatMap(c =>c);
-  allCommentSnapshots.
-  const oldestSnapshotComments = allCommentSnapshots[0];
-
-  
-  const mergedComments = oldestSnapshotComments.map(c => ({
-    commentId:c.commentId,
-    author: c.author,
-    publishedAt: c.publishedAt,
-    textContet: c.textContent,
-    classification: c.classification,
-    classifiedAt: c.classifiedAt,
-    isNew:false,
-    isDelete:false
-  }));
-
-  => Build list of comment id 
-    => 
-  => Group by comment id
-    => Cleanup
-      => Dump
-
-  if (allCommentSnapshots.length >=1) {
-    for (const commentSnapshots of allCommentSnapshots.slice(1)) {
-      for (const comment of commentSnapshots) {
-        const idx = mergedComments.findIndex(c => c.commentId === comment.commentId)
-        if (idx)   
-        // TODO find matching comment in mergtedComments by commentId
-          // if not found add it (if latest snapshot mark new)
-          // if found and same text as latest => ignore
-          // if found and different text => consider different and add it
-          
-      }
-      // TODO detect deleted!!
-     // mergedComments.ined
-    }
-  }
-
-  
-  return mergedComments;
 }
