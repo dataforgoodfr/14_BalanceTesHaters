96353bd903c1c29bb56e45c2a6a8ffcce1ad9dde
Sébastien Arod <sebastien.arod@gmail.com>
2026-03-09 10:36:59 +0100

    test(e2e-youtube): test commentId, post.publishedAt...

--- browser-extension/e2e/youtube-video-scraping.spec.ts
+++ browser-extension/e2e/youtube-video-scraping.spec.ts
@@ -5,6 +5,10 @@ import { CommentSnapshot } from "@/shared/model/PostSnapshot";
 
 test.describe("YouTube Video Scraping", () => {
   test("Test scraping video", async ({ extensionId, context }) => {
+    // Note: the scraping part (until the waitForPostStored)
+    // should ideally be moved to a beforeAll
+    // However this requires to figure out how to
+    //  install extension before the beforeAll...
     const scrapTimeout = 5 * 60 * 1000;
     test.setTimeout(scrapTimeout);
     const youtubeVideoId = "bYnBcdxT7os";
@@ -15,34 +19,62 @@ test.describe("YouTube Video Scraping", () => {
       youtubeVideoId,
     );
 
-    // Wait for analysis
+    // Wait for analysis to end
     const post = await waitForPostStored(context, youtubeVideoId, scrapTimeout);
 
+    // Test post values
     expect(post.postId).toEqual(youtubeVideoId);
     expect(post.url).toEqual(triggerResult.postUrl);
-
+    expect(post.publishedAt).toEqual({
+      type: "absolute",
+      date: "2025-07-22T00:00:00.000Z",
+    });
     expect(new Date(post.scrapedAt).getTime()).toBeLessThan(Date.now());
-    const allComments = flatten(post.comments);
-    expect(allComments.length).toBeGreaterThanOrEqual(100);
 
-    const commentWithEmojis = post.comments.find((c) =>
+    // Check comments
+    const topLevelComments = post.comments;
+    const allComments = flatten(topLevelComments);
+
+    // Test commentId captured
+    const commentForId = topLevelComments.find((c) =>
+      c.author.name.startsWith("@roots"),
+    );
+    expect(commentForId?.commentId).toBe("Ugx2oJk4syftWHsTpF54AaABAg");
+
+    // Test emojis are captured
+    const commentWithEmojis = topLevelComments.find((c) =>
       c.author.name.startsWith("@roots"),
     );
     expect(commentWithEmojis?.textContent).toContain("👏 👏");
 
-    const longComment = post.comments.find((c) =>
+    // Test long comment is fully captured
+    const longComment = topLevelComments.find((c) =>
       c.author.name.startsWith("@lucilefo"),
     );
-    // Test end of comment is captured
     expect(longComment?.textContent).toContain("quotidiennement.");
 
-    const commentWithRepliesAndLikes = post.comments.find((c) =>
+    // Test replies length match
+    const commentWithRepliesAndLikes = topLevelComments.find((c) =>
       c.author.name.startsWith("@melH6"),
     );
     expect(commentWithRepliesAndLikes?.replies.length).toBeGreaterThanOrEqual(
       3,
     );
-    expect(commentWithRepliesAndLikes?.nbLikes).toBeGreaterThanOrEqual(80);
+    expect(commentWithRepliesAndLikes?.nbLikes).toBeGreaterThanOrEqual(82);
+
+    // Test reply captured
+    const reply = commentWithRepliesAndLikes?.replies.find((c) =>
+      c.author.name.startsWith("@Natygam"),
+    );
+    expect(reply?.textContent).toContain("Comme je te comprends");
+
+    // Test that total scraped comment count matches youtube displayed count
+    const commentCountElement = await triggerResult.postPage.$(
+      "#comments #count span:nth-of-type(1)",
+    );
+    const text = (await commentCountElement?.innerText()) || "0";
+    const commentCount = Number.parseInt(text);
+    expect(allComments.length).toBe(commentCount);
   });
 });
 
