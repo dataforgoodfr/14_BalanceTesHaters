0d86872876e33fa4304c78f17b1d787f969781eb
Grégoire Co <gregoire.corbiere@gmail.com>
2026-03-17 23:43:51 +0100

    Change default model and add shared VM setting

--- backend/balanceteshaters/classification/classification_task.py
+++ backend/balanceteshaters/classification/classification_task.py
@@ -1,9 +1,8 @@
-import asyncio
 import datetime
 import logging
 from uuid import UUID
 
-from balanceteshaters.classification.slm_classifier import SLMClassifier
+from balanceteshaters.classification.batch_classifier import BatchClassifier
 from balanceteshaters.infra.database import Database
 from balanceteshaters.model.base import JobStatus
 from balanceteshaters.model.repositories import (
@@ -12,7 +11,7 @@ from balanceteshaters.model.repositories import (
 
 
 class ClassificationTask:
-    def __init__(self, db: Database, classifier: SLMClassifier):
+    def __init__(self, db: Database, classifier: BatchClassifier):
         self.logger = logging.getLogger(
             f"{__name__}.{self.__class__.__name__}",
         )
@@ -37,16 +36,13 @@ class ClassificationTask:
             await self.update_job_status(job_id, JobStatus.FAILED)
 
     async def classify_comments(self, comments: list[dict]):
-        loop = asyncio.get_event_loop()
         classifications = dict()
         for comment in comments:
             comment_id = comment["id"]
             self.logger.debug(f"Classifying comment with id {comment_id}")
 
             text = comment.get("text_content", "")
-            categories = await loop.run_in_executor(
-                None, self.classifier.classify, text
-            )
+            categories = await self.classifier.classify(text)
 
             classifications[comment_id] = {
                 "classification": categories,
