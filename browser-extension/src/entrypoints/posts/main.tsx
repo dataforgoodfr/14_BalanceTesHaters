f51bf863395cade51fa243108a89c12c3096f439
Sébastien Arod <sebastien.arod@gmail.com>
2026-03-18 19:03:55 +0100

    refactor: align entry points index.html, main.tsx ... content * Move Providers to main.tsx * Import entry point root main;css in main.tsx

--- browser-extension/src/entrypoints/posts/main.tsx
+++ browser-extension/src/entrypoints/posts/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import "@/styles/global.css";
 import App from "./App.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeInitializer } from "@/styles/ThemeInitializer.tsx";
+import "./main.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TooltipProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ThemeInitializer>
+          <App />
+        </ThemeInitializer>
+      </QueryClientProvider>
     </TooltipProvider>
   </React.StrictMode>,
 );
