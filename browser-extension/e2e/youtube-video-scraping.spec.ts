be53290c7ff3f766f614badd2ffe9778f9e4399f
Sébastien Arod <sebastien.arod@gmail.com>
2026-03-19 22:40:36 +0100

    e2e(youtube): test youtube scraping in french and english

--- browser-extension/e2e/youtube-video-scraping.spec.ts
+++ browser-extension/e2e/youtube-video-scraping.spec.ts
@@ -3,78 +3,97 @@ import { waitForPostStored } from "./utils/waitForPostStored";
 import { triggerYoutubeVideoScraping } from "./utils/triggerYoutubeVideoScraping";
 import { CommentSnapshot } from "@/shared/model/PostSnapshot";
 
-test.describe("YouTube Video Scraping", () => {
-  test("Test scraping video", async ({ extensionId, context }) => {
-    // Note: the scraping part (until the waitForPostStored)
-    // should ideally be moved to a beforeAll
-    // However this requires to figure out how to
-    //  install extension before the beforeAll...
-    const scrapTimeout = 5 * 60 * 1000;
-    test.setTimeout(scrapTimeout);
-    const youtubeVideoId = "bYnBcdxT7os";
+["en-US", "fr-FR"].forEach((locale) => {
+  test.describe("Youtube Video Scraping with Locale:" + locale, () => {
+    test.use({ locale: locale });
 
-    const triggerResult = await triggerYoutubeVideoScraping(
+    test(`Test scraping video locale:${locale}`, async ({
       extensionId,
       context,
-      youtubeVideoId,
-    );
+    }) => {
+      const emptyPage = await context.newPage();
+      const navLanguage = await emptyPage.evaluate(() => {
+        console.log("language: " + navigator.language);
+        return navigator.language;
+      });
+      expect(navLanguage).toBe(locale);
 
-    // Wait for analysis to end
-    const post = await waitForPostStored(context, youtubeVideoId, scrapTimeout);
+      // Note: the scraping part (until the waitForPostStored)
+      // should ideally be moved to a beforeAll
+      // However this requires to figure out how to
+      //  install extension before the beforeAll...
+      const youtubeVideoId = "bYnBcdxT7os";
 
-    // Test post values
-    expect(post.postId).toEqual(youtubeVideoId);
-    expect(post.url).toEqual(triggerResult.postUrl);
-    expect(post.publishedAt).toEqual({
-      type: "absolute",
-      date: "2025-07-22T00:00:00.000Z",
+      const triggerResult = await triggerYoutubeVideoScraping(
+        extensionId,
+        context,
+        youtubeVideoId,
+      );
+
+      // Wait for analysis to end
+      const scrapTimeout = 5 * 60 * 1000;
+      test.setTimeout(scrapTimeout);
+
+      const post = await waitForPostStored(
+        context,
+        youtubeVideoId,
+        scrapTimeout,
+      );
+
+      // Test post values
+      expect(post.postId).toEqual(youtubeVideoId);
+      expect(post.url).toEqual(triggerResult.postUrl);
+      expect(post.publishedAt).toEqual({
+        type: "absolute",
+        date: "2025-07-22T00:00:00.000Z",
+      });
+      expect(new Date(post.scrapedAt).getTime()).toBeLessThan(Date.now());
+
+      // Check comments
+      const topLevelComments = post.comments;
+      const allComments = flatten(topLevelComments);
+
+      // Test commentId captured
+      const commentForId = topLevelComments.find((c) =>
+        c.author.name.startsWith("@roots"),
+      );
+      expect(commentForId?.commentId).toBe("Ugx2oJk4syftWHsTpF54AaABAg");
+
+      // Test emojis are captured
+      const commentWithEmojis = topLevelComments.find((c) =>
+        c.author.name.startsWith("@roots"),
+      );
+      expect(commentWithEmojis?.textContent).toContain("👏 👏");
+
+      // Test long comment is fully captured
+      const longComment = topLevelComments.find((c) =>
+        c.author.name.startsWith("@lucilefo"),
+      );
+      expect(longComment?.textContent).toContain("quotidiennement.");
+
+      // Test replies length match
+      const commentWithRepliesAndLikes = topLevelComments.find((c) =>
+        c.author.name.startsWith("@melH6"),
+      );
+      expect(commentWithRepliesAndLikes?.replies.length).toBeGreaterThanOrEqual(
+        3,
+      );
+      expect(commentWithRepliesAndLikes?.nbLikes).toBeGreaterThanOrEqual(82);
+
+      // Test reply captured
+      const reply = commentWithRepliesAndLikes?.replies.find((c) =>
+        c.author.name.startsWith("@Natygam"),
+      );
+      expect(reply?.textContent).toContain("Comme je te comprends");
+
+      // Test that total scraped comment count matches youtube displayed count
+      const commentCountElement = await triggerResult.postPage.$(
+        "#comments #count span:nth-of-type(1)",
+      );
+      const text = (await commentCountElement?.innerText()) || "0";
+      const commentCount = Number.parseInt(text);
+      expect(allComments.length).toBe(commentCount);
     });
-    expect(new Date(post.scrapedAt).getTime()).toBeLessThan(Date.now());
-
-    // Check comments
-    const topLevelComments = post.comments;
-    const allComments = flatten(topLevelComments);
-
-    // Test commentId captured
-    const commentForId = topLevelComments.find((c) =>
-      c.author.name.startsWith("@roots"),
-    );
-    expect(commentForId?.commentId).toBe("Ugx2oJk4syftWHsTpF54AaABAg");
-
-    // Test emojis are captured
-    const commentWithEmojis = topLevelComments.find((c) =>
-      c.author.name.startsWith("@roots"),
-    );
-    expect(commentWithEmojis?.textContent).toContain("👏 👏");
-
-    // Test long comment is fully captured
-    const longComment = topLevelComments.find((c) =>
-      c.author.name.startsWith("@lucilefo"),
-    );
-    expect(longComment?.textContent).toContain("quotidiennement.");
-
-    // Test replies length match
-    const commentWithRepliesAndLikes = topLevelComments.find((c) =>
-      c.author.name.startsWith("@melH6"),
-    );
-    expect(commentWithRepliesAndLikes?.replies.length).toBeGreaterThanOrEqual(
-      3,
-    );
-    expect(commentWithRepliesAndLikes?.nbLikes).toBeGreaterThanOrEqual(82);
-
-    // Test reply captured
-    const reply = commentWithRepliesAndLikes?.replies.find((c) =>
-      c.author.name.startsWith("@Natygam"),
-    );
-    expect(reply?.textContent).toContain("Comme je te comprends");
-
-    // Test that total scraped comment count matches youtube displayed count
-    const commentCountElement = await triggerResult.postPage.$(
-      "#comments #count span:nth-of-type(1)",
-    );
-    const text = (await commentCountElement?.innerText()) || "0";
-    const commentCount = Number.parseInt(text);
-    expect(allComments.length).toBe(commentCount);
   });
 });
 
