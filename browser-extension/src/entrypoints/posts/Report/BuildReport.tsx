989f839822e1e8140aca76ccbb60bad792bbfe1b
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-03-20 11:07:00 +0100

    chore: reorganize code

--- browser-extension/src/entrypoints/posts/Report/BuildReport.tsx
+++ browser-extension/src/entrypoints/posts/Report/BuildReport.tsx
@@ -4,15 +4,14 @@ import { defineStepper, Get } from "@stepperize/react";
 import { StepStatus, useStepItemContext } from "@stepperize/react/primitives";
 
 import { Button } from "@/components/ui/button";
-import { Check, CircleCheck, Move, MoveLeft, MoveRight } from "lucide-react";
-import Step1Plateforme from "./Step1Plateforme";
-import { Link } from "react-router";
-import { Separator } from "@/components/ui/separator";
+import { Check, MoveLeft, MoveRight } from "lucide-react";
+import Step1Plateforme, { Step1PlateformeSchema } from "./Step1Plateforme";
 
-const { Stepper, ...stepperDefinition } = defineStepper(
+const { Scoped, Stepper, useStepper, ...stepperDefinition } = defineStepper(
   {
     id: "step-1",
     title: "Plateforme",
+    schema: Step1PlateformeSchema,
   },
   {
     id: "step-2",
@@ -100,6 +99,103 @@ const StepperSeparatorWithLabelOrientation = ({
   );
 };
 
+const StepperBanner = () => {
+  const stepper = useStepper();
+
+  return (
+    <Stepper.List className="flex list-none gap-2 flex-row items-center justify-between">
+      {stepper.state.all.map((stepData, index) => {
+        const currentIndex = stepper.state.current.index;
+        let status = "inactive";
+        if (index < currentIndex) {
+          status = "success";
+        } else if (index === currentIndex) {
+          status = "active";
+        }
+        const isLast = index === stepper.state.all.length - 1;
+        const data = stepData as {
+          id: string;
+          title: string;
+          description?: string;
+        };
+        return (
+          <Stepper.Item
+            key={stepData.id}
+            step={stepData.id}
+            className="group peer relative flex w-full flex-col items-center justify-center gap-2"
+          >
+            <StepperTriggerWrapper />
+            <StepperSeparatorWithLabelOrientation
+              status={status as StepStatus}
+              isLast={isLast}
+            />
+            <div className="flex flex-col items-center text-center gap-1">
+              <StepperTitleWrapper title={data.title} />
+              <StepperDescriptionWrapper description={data.description} />
+            </div>
+          </Stepper.Item>
+        );
+      })}
+    </Stepper.List>
+  );
+};
+
+const StepperActions = () => {
+  const stepper = useStepper();
+  return (
+    <div className="sticky bottom-0 border-t pt-4">
+      <Stepper.Actions className="flex justify-center gap-6">
+        {!stepper.state.isLast && (
+          <Stepper.Prev
+            render={(domProps) => (
+              <Button type="button" className="w-1/6" {...domProps}>
+                <MoveLeft className="h-4 w-4 mr-1" /> Précédent
+              </Button>
+            )}
+          />
+        )}
+        {stepper.state.isLast ? (
+          <Button
+            type="button"
+            className="w-1/6"
+            onClick={() => stepper.navigation.reset()}
+          >
+            Générer le rapport
+          </Button>
+        ) : (
+          <Stepper.Next
+            render={(domProps) => (
+              <Button type="button" className="w-1/6" {...domProps}>
+                Suivant <MoveRight className="h-4 w-4 ml-1" />
+              </Button>
+            )}
+          />
+        )}
+      </Stepper.Actions>
+    </div>
+  );
+};
+
+const StepContent = () => {
+  const stepper = useStepper();
+  return (
+    <div>
+      {stepper.flow.when("step-1", () => (
+        <Step1Plateforme />
+      ))}
+      {stepper.flow.when("step-2", () => (
+        <p></p>
+      ))}
+      {stepper.flow.when("step-3", () => (
+        <p></p>
+      ))}
+      {stepper.flow.when("step-4", () => (
+        <p></p>
+      ))}
+    </div>
+  );
+};
+
 export function BuildReport() {
   return (
     <div className="p-4 flex flex-col gap-6 w-full ">
@@ -109,105 +205,17 @@ export function BuildReport() {
         Suivez les 4 étapes pour constituer un rapport et exportez-le dans un
         format souhaité.
       </p>
-      <Stepper.Root
-        className="w-full h-full space-y-4"
-        orientation="horizontal"
-      >
-        {({ stepper }) => (
-          <>
-            <Stepper.List className="flex list-none gap-2 flex-row items-center justify-between">
-              {stepper.state.all.map((stepData, index) => {
-                const currentIndex = stepper.state.current.index;
-                let status = "inactive";
-                if (index < currentIndex) {
-                  status = "success";
-                } else if (index === currentIndex) {
-                  status = "active";
-                }
-                const isLast = index === stepper.state.all.length - 1;
-                const data = stepData as {
-                  id: string;
-                  title: string;
-                  description?: string;
-                };
-                return (
-                  <Stepper.Item
-                    key={stepData.id}
-                    step={stepData.id}
-                    className="group peer relative flex w-full flex-col items-center justify-center gap-2"
-                  >
-                    <StepperTriggerWrapper />
-                    <StepperSeparatorWithLabelOrientation
-                      status={status as StepStatus}
-                      isLast={isLast}
-                    />
-                    <div className="flex flex-col items-center text-center gap-1">
-                      <StepperTitleWrapper title={data.title} />
-                      <StepperDescriptionWrapper
-                        description={data.description}
-                      />
-                    </div>
-                  </Stepper.Item>
-                );
-              })}
-            </Stepper.List>
-            {stepper.flow.switch({
-              "step-1": () => <Step1Plateforme />,
-              "step-2": (data) => <Content id={data.id} />,
-              "step-3": (data) => <Content id={data.id} />,
-              "step-4": (data) => <Content id={data.id} />,
-            })}
 
-            {/* Footer */}
-            <div className="sticky bottom-0 border-t pt-4">
-              <Stepper.Actions className="flex justify-center gap-6">
-                {!stepper.state.isLast && (
-                  <Stepper.Prev
-                    render={(domProps) => (
-                      <Button type="button" className="w-1/6" {...domProps}>
-                       <MoveLeft className="h-4 w-4 mr-1" /> Précédent 
-                      </Button>
-                    )}
-                  />
-                )}
-                {stepper.state.isLast ? (
-                  <Button
-                    type="button"
-                    className="w-1/6"
-                    onClick={() => stepper.navigation.reset()}
-                  >
-                    Générer le rapport
-                  </Button>
-                ) : (
-                  <Stepper.Next
-                    render={(domProps) => (
-                      <Button type="button" className="w-1/6" {...domProps}>
-                        Suivant <MoveRight className="h-4 w-4 ml-1" />
-                      </Button>
-                    )}
-                  />
-                )}
-              </Stepper.Actions>
-            </div>
-          </>
-        )}
-      </Stepper.Root>
+      <Scoped>
+        <Stepper.Root
+          className="w-full h-full space-y-4"
+          orientation="horizontal"
+        >
+          <StepperBanner />
+          <StepContent />
+          <StepperActions />
+        </Stepper.Root>
+      </Scoped>
     </div>
   );
 }
-
-const Content = ({ id }: { id: Get.Id<typeof stepperDefinition.steps> }) => {
-  return (
-    <Stepper.Content
-      step={id}
-      render={(props) => (
-        <div
-          {...props}
-          className="h-[200px] content-center rounded border bg-secondary text-secondary-foreground p-8"
-        >
-          <p className="text-xl font-normal">Content for {id}</p>
-        </div>
-      )}
-    />
-  );
-};
