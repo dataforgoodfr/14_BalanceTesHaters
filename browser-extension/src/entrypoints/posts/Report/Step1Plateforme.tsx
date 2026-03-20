10e9a3b8975d51e61171deaaf0599522ae7ab39f
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-03-20 11:07:00 +0100

    chore: reorganize code

--- browser-extension/src/entrypoints/posts/Report/Step1Plateforme.tsx
+++ browser-extension/src/entrypoints/posts/Report/Step1Plateforme.tsx
@@ -1,6 +1,13 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { Field, FieldGroup } from "@/components/ui/field";
 import { Label } from "@/components/ui/label";
+import { SocialNetworkNameSchema } from "@/shared/model/SocialNetworkName";
+
+import { z } from "zod";
+
+export const Step1PlateformeSchema = z.object({
+  socialNetwork: z.array(SocialNetworkNameSchema).min(1, "Sélectionnez au moins une plateforme")    ,
+});
 
 function Step1Plateforme() {
   return (
@@ -12,11 +19,11 @@ function Step1Plateforme() {
 
       <FieldGroup className="mt-2 w-1/12 mx-auto items-center gap-5">
         <Field orientation="horizontal" className="">
-          <Checkbox id="YOUTUBE" name="YOUTUBE" />
+          <Checkbox id="YOUTUBE" name="socialNetwork" />
           <Label htmlFor="YOUTUBE">YouTube</Label>
         </Field>
         <Field orientation="horizontal" className="">
-          <Checkbox id="INSTAGRAM" name="INSTAGRAM" />
+          <Checkbox id="INSTAGRAM" name="socialNetwork" />
           <Label htmlFor="INSTAGRAM">Instagram</Label>
         </Field>
       </FieldGroup>
