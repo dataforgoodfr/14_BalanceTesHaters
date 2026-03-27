3c8482a924a9088e00666794210082edb32f4b67
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-03-27 18:26:45 +0100

    format

--- browser-extension/src/shared/model/SocialNetworkName.ts
+++ browser-extension/src/shared/model/SocialNetworkName.ts
@@ -2,8 +2,11 @@ import { z } from "zod";
 
 // SocialNetworkName Schema
 export enum SocialNetwork {
-    YouTube = "YOUTUBE",
-    Instagram = "INSTAGRAM"
+  YouTube = "YOUTUBE",
+  Instagram = "INSTAGRAM",
 }
-export const SocialNetworkNameSchema = z.enum([SocialNetwork.YouTube, SocialNetwork.Instagram]);
+export const SocialNetworkNameSchema = z.enum([
+  SocialNetwork.YouTube,
+  SocialNetwork.Instagram,
+]);
 export type SocialNetworkName = z.infer<typeof SocialNetworkNameSchema>;
