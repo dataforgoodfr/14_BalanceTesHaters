25885d572ea31eea6e5d6d18a7d45231e001ffc1
njouanin <nicolas.jouanin@gmail.com>
2026-04-07 21:36:09 +0200

    Remove unused function

--- browser-extension/src/lib/utils.ts
+++ browser-extension/src/lib/utils.ts
@@ -2,13 +2,5 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function tryOrElse<T>(tryFn: () => T, elseValue: T): T {
-  try {
-    return tryFn();
-  } catch {
-    return elseValue;
-  }
+    return twMerge(clsx(inputs));
 }
