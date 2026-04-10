6055e1dd12b5f4aed0f37f723b4bbc8d88db7ad9
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-04-10 20:01:42 +0200

    format

--- browser-extension/src/entrypoints/posts/Home/HomePage.tsx
+++ browser-extension/src/entrypoints/posts/Home/HomePage.tsx
@@ -55,7 +55,9 @@ function HomePage() {
       {allComments.length === 0 && !isLoading && (
         <div className="flex flex-col items-center max-w-xs mx-auto gap-1 ">
           <FolderCode className="bg-secondary p-2 w-8 h-8 rounded-md" />
-          <div className="text-primary font-semibold">Aucune publication analysée</div>
+          <div className="text-primary font-semibold">
+            Aucune publication analysée
+          </div>
           <div className="text-muted-foreground">
             Modifie la période ou lance une nouvelle analyse sur une publication
             YouTube/Instagram.
