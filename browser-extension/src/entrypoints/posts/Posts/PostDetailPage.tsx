aba46aeae4ca2310f9063d6597fc596246fe2777
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-04-10 08:54:04 +0200

    format

--- browser-extension/src/entrypoints/posts/Posts/PostDetailPage.tsx
+++ browser-extension/src/entrypoints/posts/Posts/PostDetailPage.tsx
@@ -30,7 +30,6 @@ function PostDetailPage() {
     queryFn: () => getPostByPostId(socialNetworkName, postId),
   });
 
-
   const allComments = post?.comments || [];
   const hatefulComments = allComments
     .filter((c) => isCommentHateful(c))
@@ -42,7 +41,7 @@ function PostDetailPage() {
       } as PostCommentWithId;
     });
 
-   const numberOfHatefulComments = hatefulComments.length;
+  const numberOfHatefulComments = hatefulComments.length;
 
   return (
     <div className="p-4 flex flex-col gap-6 w-5/6">
