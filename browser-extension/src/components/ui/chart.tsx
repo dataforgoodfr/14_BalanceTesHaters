e2b61daf9f9631eac0111e67ec9ae3d6169bd829
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-04-12 20:12:55 +0200

    disable errors on imported component

--- browser-extension/src/components/ui/chart.tsx
+++ browser-extension/src/components/ui/chart.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import * as React from "react";
 import * as RechartsPrimitive from "recharts";
 import type { TooltipValueType } from "recharts";
