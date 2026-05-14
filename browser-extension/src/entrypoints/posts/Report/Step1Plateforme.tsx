422f302fabc0e4e2c6a4f87ef11e517730a00e4f
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-05-14 09:32:30 +0200

    feat: update step 1

--- browser-extension/src/entrypoints/posts/Report/Step1Plateforme.tsx
+++ browser-extension/src/entrypoints/posts/Report/Step1Plateforme.tsx
@@ -1,11 +1,13 @@
 import { Checkbox } from "@/components/ui/checkbox";
-import { Field, FieldGroup } from "@/components/ui/field";
+import { Field } from "@/components/ui/field";
 import { Label } from "@/components/ui/label";
 import { useStepper, ReportQueryData } from "./BuildReport"; // or wherever the export is
 import { useForm } from "@tanstack/react-form";
 import { SocialNetwork } from "@/shared/model/SocialNetworkName";
 import { getFormId } from "./StepperComponents";
 import { StepHeader } from "./StepHeader";
+import youtubeLogoUrl from "~/assets/youtube-logo.svg";
+import instagramLogoUrl from "~/assets/instagram-logo.svg";
 
 function Step1Plateforme({
   reportQueryData,
@@ -26,9 +28,9 @@ function Step1Plateforme({
     },
   });
 
-  const options: { id: string; label: string }[] = [
-    { id: SocialNetwork.Instagram, label: "Instagram" },
-    { id: SocialNetwork.YouTube, label: "YouTube" },
+  const options: { id: string; label: string; url: string }[] = [
+    { id: SocialNetwork.YouTube, label: "YouTube", url: youtubeLogoUrl },
+    { id: SocialNetwork.Instagram, label: "Instagram", url: instagramLogoUrl },
   ];
 
   return (
@@ -57,27 +59,42 @@ function Step1Plateforme({
           }}
         >
           {(field) => (
-            <FieldGroup className="mt-2 gap-5 max-w-2xs w-2/3 ">
+            <div className="flex items-center  mt-2 gap-4  ">
               {options.map((option) => (
-                <Field orientation="horizontal" key={option.id}>
-                  <Checkbox
-                    id={option.id}
-                    checked={field.state.value.includes(option.id)}
-                    onCheckedChange={(checked) => {
-                      const currentValue = field.state.value;
-                      const nextValue = checked
-                        ? [...currentValue, option.id]
-                        : currentValue.filter((val) => val !== option.id);
+                <Field
+                  orientation="horizontal"
+                  key={option.id}
+                  className=" has-[[aria-checked=true]]:bg-selected has-[[aria-checked=true]]:border-selected-accent"
+                >
+                  <Label className="w-[256px] justify-center border border-border rounded-md p-4">
+                    <Checkbox
+                      id={option.id}
+                      className="hidden"
+                      checked={field.state.value.includes(option.id)}
+                      onCheckedChange={(checked) => {
+                        const currentValue = field.state.value;
+                        const nextValue = checked
+                          ? [...currentValue, option.id]
+                          : currentValue.filter((val) => val !== option.id);
 
-                      field.handleChange(nextValue);
-                    }}
-                  />
-                  <Label
+                        field.handleChange(nextValue);
+                      }}
+                    />
+                    <div className="flex flex-col items-center gap-3 ">
+                      <img
+                        src={option.url}
+                        className="w-8 h-8"
+                        alt="Logo"
+                      ></img>
+                      <span className="font-semibold">{option.label}</span>
+                    </div>
+                  </Label>
+                  {/* <Label
                     htmlFor={option.id}
                     className="font-normal cursor-pointer"
                   >
                     {option.label}
-                  </Label>
+                  </Label> */}
                 </Field>
               ))}
 
@@ -87,7 +104,7 @@ function Step1Plateforme({
                   {field.state.meta.errors.join(", ")}
                 </span>
               )}
-            </FieldGroup>
+            </div>
           )}
         </form.Field>
       </form>
