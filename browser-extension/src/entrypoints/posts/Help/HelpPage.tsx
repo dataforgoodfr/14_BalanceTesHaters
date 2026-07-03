44fe4987a7ed5b7d4d125fb41ad225390b7e172f
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2026-07-03 11:09:41 +0200

    feat: ajouter contacts d'urgence

--- browser-extension/src/entrypoints/posts/Help/HelpPage.tsx
+++ browser-extension/src/entrypoints/posts/Help/HelpPage.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Link } from "react-router";
 import type { ComponentType, ReactNode } from "react";
-import { Card, CardContent} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import PageHeader from "../Shared/PageHeader";
 import {
   BookOpenTextIcon,
   FileUser,
+  Phone,
   SearchIcon,
   ShieldCheck,
 } from "lucide-react";
@@ -23,16 +24,32 @@ type HelpLinkProps = {
   children?: ReactNode;
 };
 
-const HelpLink = ({ to, Icon, title, children }: HelpLinkProps) => (
+const HelpMenuLink = ({ to, Icon, title, children }: HelpLinkProps) => (
   <Link to={to} className="block w-1/3">
     <Card className="h-full hover:bg-selected hover:border-selected-accent dark:hover:bg-gray-700 cursor-pointer transition-colors">
       <CardContent className="flex flex-col items-center text-center gap-1 py-2">
         <Icon className="text-center w-full size-5 text-muted-foreground" />
         <span className="font-semibold text-sm">{title}</span>
-        <span className="text-sm text-muted-foreground dark:text-gray-400">{children}</span>
+        <span className="text-sm text-muted-foreground dark:text-gray-400">
+          {children}
+        </span>
       </CardContent>
     </Card>
   </Link>
+);
+
+type HelpTelNumberProps = {
+  number: string;
+  children: ReactNode;
+};
+const HelpTelNumber = ({ number, children }: HelpTelNumberProps) => (
+  <div className="w-1/2 bg-gray-50 shadow border-none flex items-center text-center rounded-lg py-2 px-6">
+      <Phone className="text-primary me-1 size-4" />
+      <span className="text-primary text-[20px] font-semibold me-3">
+        {number}
+      </span>
+      <span className="text-sm text-muted-foreground ">{children}</span>
+  </div>
 );
 
 function HelpPage() {
@@ -61,18 +78,48 @@ function HelpPage() {
       </div>
       {/* <div className="bg-[url(/src/assets/help-gradient.png)] w-full h-49 bg-contain "></div> */}
 
-      <div className="flex gap-3">
-        <HelpLink to="/help/product" Icon={BookOpenTextIcon} title="Utiliser Balance tes Haters">
+      <div className="flex gap-3 w-full">
+        <HelpMenuLink
+          to="/help/product"
+          Icon={BookOpenTextIcon}
+          title="Utiliser Balance tes Haters"
+        >
           Informations sur l’outil et guide d’utilisation
-        </HelpLink>
+        </HelpMenuLink>
 
-        <HelpLink to="/help/harrasement" Icon={ShieldCheck} title="Cyberharcèlement et actions">
+        <HelpMenuLink
+          to="/help/harrasement"
+          Icon={ShieldCheck}
+          title="Cyberharcèlement et actions"
+        >
           Ressources, conseils et démarches pour agir et se faire accompagner
-        </HelpLink>
+        </HelpMenuLink>
 
-        <HelpLink to="/help/privacy-policy" Icon={FileUser} title="Données personnelles">
+        <HelpMenuLink
+          to="/help/privacy-policy"
+          Icon={FileUser}
+          title="Données personnelles"
+        >
           Informations sur la collecte, l’usage et la sécurité des données
-        </HelpLink>
+        </HelpMenuLink>
+      </div>
+
+      <p className="font-semibold text-lg">Contacts d'urgence</p>
+      <div className="flex flex-col w-full gap-2">
+        <div className="flex gap-3">
+          <HelpTelNumber number="17">Police Secours</HelpTelNumber>
+          <HelpTelNumber number="3018">
+            Aide aux victimes de cyberharcèlement
+          </HelpTelNumber>
+        </div>
+        <div className="flex gap-3">
+          <HelpTelNumber number="116006">
+            Aide aux victimes d'une infraction
+          </HelpTelNumber>
+          <HelpTelNumber number="3919">
+            Aide aux femmes victimes de violences
+          </HelpTelNumber>
+        </div>
       </div>
     </main>
   );
