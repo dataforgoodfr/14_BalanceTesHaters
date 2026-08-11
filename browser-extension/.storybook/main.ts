c44cef6cafd5e0cc29797a4c89e888d8c290f801
Sébastien Arod <sebastien.arod@gmail.com>
2026-08-11 12:04:16 +0200

    refactor(build): use native vite resolve.tsconfigPaths=true

--- browser-extension/.storybook/main.ts
+++ browser-extension/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { resolve } from "dns";
 import path from "path";
 import { mergeConfig } from "vite";
 
@@ -8,9 +9,12 @@ const config: StorybookConfig = {
   framework: "@storybook/react-vite",
   viteFinal: async (config) => {
     const { default: tailwindcss } = await import("@tailwindcss/vite");
-    const { default: tsconfigPaths } = await import("vite-tsconfig-paths");
+
     return mergeConfig(config, {
-      plugins: [tailwindcss(), tsconfigPaths()],
+      plugins: [tailwindcss()],
+      resolve: {
+        tsconfigPaths: true,
+      },
     });
   },
 };
